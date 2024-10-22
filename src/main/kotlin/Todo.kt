b1ef3eb36c4f935b52a1d23cbf48cc01b03853b0
Sergey Petrunin <halfstrik@gmail.com>
2024-10-21 21:17:14 -0400

    Fix conversion between camelCase in code and snake_case in DB

--- src/main/kotlin/Todo.kt
+++ src/main/kotlin/Todo.kt
@@ -1,12 +1,18 @@
 package com.sergeypetrunin
 
-import klite.TSID
+import klite.*
 import klite.jdbc.BaseEntity
 import java.time.Instant
 
 typealias Id<T> = TSID<T>
 data class Todo(
     val item: String,
-    val completed_at: Instant? = null, // TODO: Fix if possible
+    val completedAt: Instant? = null,
     override val id: Id<Todo> = Id()
 ): BaseEntity<Id<Todo>>
+
+fun Todo.persister1(): Map<String, Any?> {
+    println("Help")
+    return this.toValuesSkipping(Todo::completedAt) + mapOf("completed_at" to this.completedAt)
+}
+// TODO: Overwrite backward conversion, currently completedAt returns as null always
--- src/main/kotlin/TodoRepository.kt
+++ src/main/kotlin/TodoRepository.kt
@@ -6,12 +6,14 @@ import javax.sql.DataSource
 
 class TodoRepository(db: DataSource): BaseCrudRepository<Todo, Id<Todo>>(db, "todos") {
     override val orderAsc get() = "order by $table.completed_at"
+    override fun Todo.persister(): Map<String, Any?> = this.persister1()
+
     // private val todos = mutableListOf<Todo>()
     // fun list() = todos.toList()
     // fun save(todo: Todo) = todos.add(todo)
     // fun get(id: Id<Todo>) = todos.first { it.id == id }
 
     // fun list() = db.select<Todo>("todos")
-    // fun save(todo: Todo) = db.upsert("todos", todo.toValues())
+    // fun save1(todo: Todo) = db.upsert("todos", todo.persister())
     // fun get(id: Id<Todo>) = db.select<Todo>("todos", Todo::id to id).first()
 }
